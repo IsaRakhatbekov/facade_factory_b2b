af8f095b348c73566f14ba07659807d9a7b76a74
Isa <isarahatbekov250@gmail.com>
2026-03-04 16:26:15 +0600

    adaptive of hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import Footer from '@/src/components/Footer/Footer'
 import Header from '@/src/components/Header/Header'
 import IntroOverlay from '@/src/components/IntroOverlay/IntroOverlay'
-import Designers from '../components/sections/Designers/Designers'
-import Form from '../components/sections/Form/Form'
 import Hero from '../components/sections/Hero/Hero'
-import Industries from '../components/sections/Industries/Industries'
-import ProblemsSolutions from '../components/sections/ProblemsSolutions/ProblemsSolutions'
-import Standards from '../components/sections/Standards/Standards'
 
 export default function Home() {
 	return (
@@ -15,11 +10,11 @@ export default function Home() {
 			<Header />
 			<main>
 				<Hero />
-				<ProblemsSolutions />
-				<Industries />
-				<Designers />
-				<Standards />
-				<Form />
+				{/* <ProblemsSolutions /> */}
+				{/* <Industries /> */}
+				{/* <Designers /> */}
+				{/* <Standards /> */}
+				{/* <Form /> */}
 			</main>
 			<Footer />
 		</div>
